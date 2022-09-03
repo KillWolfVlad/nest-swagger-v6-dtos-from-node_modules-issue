9412ecd1360712727265aa85aa4a8ef1d43dc967
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2022-09-03 16:58:37 +0500

    feat: add initial source

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { UserDto } from "@nest-swagger-v6-dtos-from-node-modules-issue/dtos";
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getUser(): Promise<UserDto> {
+    return Promise.resolve({
+      id: "1",
+      name: "user",
+      email: "user@example.com",
+      version: 2,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { INestApplication } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+import { AppModule } from "./app.module";
+
+function setupSwagger(app: INestApplication) {
+  const options = new DocumentBuilder().build();
+
+  SwaggerModule.setup("api", app, SwaggerModule.createDocument(app, options));
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
